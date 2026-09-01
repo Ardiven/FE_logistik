c34e839eba3f2842bfd8a4ec0cb6d274a87674b8
ardiven <emailkamu@example.com>
2026-09-02 03:04:04 +0700

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -71,7 +71,7 @@ const Navigation = () => {
 function App() {
   return (
     <AuthProvider>
-      <Router>
+      <Router basename={import.meta.env.BASE_URL}>
         <div className="min-h-screen flex flex-col font-sans text-tps-dark bg-tps-cream">
           <Navigation />
           <main className="flex-grow w-full max-w-7xl mx-auto py-6 sm:px-6 lg:px-8">
