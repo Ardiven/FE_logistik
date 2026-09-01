6c8f4c9e8e049e87eb18005340903d08e2ab11f7
ardiven <emailkamu@example.com>
2026-09-02 02:00:40 +0700

    first commit

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const API = axios.create({
-  baseURL: 'https://tps.petra.ac.id/logistik/api',
+  baseURL: 'https://tps.petra.ac.id/api',
 });
 
 API.interceptors.request.use((config) => {
